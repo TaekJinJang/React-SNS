60a01fe1d471f78d5449d2270efb6d3fb12711fd
Taekjin Jang <xorwls1500@naver.com>
2022-05-24 19:15:25 +0900

    프로젝트 시작

---
+++ front/pages/about/taekjin.js
@@ -0,0 +1,7 @@
+// import React from 'react'; // Next는 이 구문이 없어도 됌 !
+
+const taekjin = () => {
+  return <div>Hello, taekJin!</div>;
+};
+
+export default taekjin;
---
+++ front/pages/index.js
@@ -0,0 +1,13 @@
+// import React from 'react'; // Next는 이 구문이 없어도 됌 !
+
+import AppLayout from "../components/AppLayout";
+
+const Home = () => {
+  return (
+    <AppLayout>
+      <div>Hello, Next!</div>
+    </AppLayout>
+  );
+};
+
+export default Home;
