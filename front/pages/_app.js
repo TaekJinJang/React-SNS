3e266fb4c527d1fc9e7ffbfda245e9ccd8f403e7
TaekJin Jang <xorwls1500@naver.com>
2022-05-28 20:01:45 +0900

    _app과 AppLayout 차이 주석

---
+++ front/pages/_app.js
@@ -0,0 +1,25 @@
+import "antd/dist/antd.css";
+import { PropTypes } from "prop-types";
+import Head from "next/head"; // next에서 제공하는 head 태그
+
+// pages 폴더의 공통 부분, import도 다 포함
+// 특정 컴포넌트들에만 공통으로 들어간다면 components 폴더에 AppLayout 같은 컴포넌트를 만들어서 사용하고 모든 페이지에 공통적으로 사용된다면 _app.js 형식으로 사용
+
+const App = ({ Component }) => {
+  return (
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <title>NodeBird</title>
+      </Head>
+      <Component />
+    </>
+  );
+};
+
+App.prototype = {
+  // 프로토타입은 귀찮긴하지만 안정화를 위해  하는게 좋음
+  Component: PropTypes.elementType.isRequired,
+};
+
+export default App;
