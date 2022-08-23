3a844f039ecfbad7ad0bdc796cb6da3f74d97248
Taekjin Jang <xorwls1500@naver.com>
2022-08-23 17:56:48 +0900

    리덕스 개발

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -1,6 +1,7 @@
 import "antd/dist/antd.css";
 import { PropTypes } from "prop-types";
 import Head from "next/head"; // next에서 제공하는 head 태그
+import wrapper from "../store/configureStore";
 
 // pages 폴더의 공통 부분, import도 다 포함
 // 특정 컴포넌트들에만 공통으로 들어간다면 components 폴더에 AppLayout 같은 컴포넌트를 만들어서 사용하고 모든 페이지에 공통적으로 사용된다면 _app.js 형식으로 사용
@@ -22,4 +23,4 @@ App.prototype = {
   Component: PropTypes.elementType.isRequired,
 };
 
-export default App;
+export default wrapper.withRedux(App);
